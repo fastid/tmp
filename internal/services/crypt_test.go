f00fbc14b79c14e8d12e16ee56cf39e0be9939ad
Kostya Ten <kostya@yandex.ru>
2022-11-20 20:49:06 +0300

    pref: Crypto module

--- internal/services/crypt_test.go
+++ internal/services/crypt_test.go
@@ -20,7 +20,7 @@ func TestCrypt(t *testing.T) {
 	require.NotEmpty(t, cipher)
 }
 
-func TestCryptEncrypt(t *testing.T) {
+func TestCryptEncryptDecryptB64(t *testing.T) {
 	cfg, _ := config.New("../../configs/fastid.yml")
 
 	// Logger
@@ -36,6 +36,26 @@ func TestCryptEncrypt(t *testing.T) {
 	require.NotEmpty(t, encrypted)
 
 	result, err := crypt.DecryptBase64(encrypted)
+	require.NoError(t, err)
+	require.Equal(t, result, "Hello")
+}
+
+func TestCryptEncryptDecrypt(t *testing.T) {
+	cfg, _ := config.New("../../configs/fastid.yml")
+
+	// Logger
+	log := logger.New(cfg)
+
+	crypt := NewCryptService(cfg, log)
+	secretKey, err := crypt.GenerateCipher()
+	require.NoError(t, err)
+	crypt.SecretKey(secretKey)
+
+	encrypted, err := crypt.Encrypt("Hello")
+	require.NoError(t, err)
+	require.NotEmpty(t, encrypted)
+
+	result, err := crypt.Decrypt(encrypted)
 	require.NoError(t, err)
 	require.Equal(t, result, "Hello")
 }
